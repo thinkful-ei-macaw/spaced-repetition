5d855a4d0b5b372b1f6d728660e21839af7add86
Sara R Mills <SaraRMills@gmail.com>
2020-05-21 13:50:37 -0400

    update server endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  REACT_APP_API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT, 
-  
+  REACT_APP_API_ENDPOINT: 'https://intense-stream-48886.herokuapp.com/api',
   REACT_APP_TOKEN_KEY: process.env.REACT_APP_TOKEN_KEY,
 }
